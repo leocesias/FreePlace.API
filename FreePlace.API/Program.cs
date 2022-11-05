63e5cb5339a31c862b3cb08ec5bf72feb37abdf9
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2022-11-05 01:07:03 -0500

    Added API Documentation

--- FreePlace.API/Program.cs
+++ FreePlace.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddSwaggerGen(options =>
+    {
+        // Add API Documentation Information
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "FreePlace API",
+            Description = "FreePlace RESTful API",
+            TermsOfService = new Uri("https://freeplace.com"),
+            Contact = new OpenApiContact
+            {
+                Name = "FreePlace.studio",
+                Url = new Uri("https://FreePlace.studio")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "FreePlace Resources License",
+                Url = new Uri("https://FreePlace.com/license")
+            }
+        });
+        options.EnableAnnotations();
+    }
+);
 
 var app = builder.Build();
 
