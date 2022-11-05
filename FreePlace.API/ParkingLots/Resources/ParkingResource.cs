431517c2d0787f83bf5cb019ab09064ebfd2d73a
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2022-11-05 05:22:21 -0500

    Added parking resources

---
+++ FreePlace.API/ParkingLots/Resources/ParkingResource.cs
@@ -0,0 +1,10 @@
+﻿namespace FreePlace.API.Shared.Resources;
+
+public class ParkingResource
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public string Ubication { get; set; }
+    public float Price { get; set; }
+    public int Capacity { get; set; }
+}
---
+++ FreePlace.API/ParkingLots/Resources/SaveParkingResource.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreePlace.API.Shared.Resources;
+
+public class SaveParkingResource
+{
+    [Required]
+    [MaxLength(250)]
+    public string Description { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string Ubication { get; set; }
+    
+    [Required]
+    public float Price { get; set; }
+    
+    [Required]
+    public int Capacity { get; set; }
+    
+}
