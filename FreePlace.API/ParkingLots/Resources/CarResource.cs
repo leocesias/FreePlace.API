470026deec6ef8b46bfee377b0ba417d118a5ea7
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2022-11-05 04:40:17 -0500

    feat(car): added CarResource and SaveCarResource

---
+++ FreePlace.API/ParkingLots/Resources/CarResource.cs
@@ -0,0 +1,8 @@
+﻿namespace FreePlace.API.Shared.Resources;
+
+public class CarResource
+{
+    public int Id { get; set; }
+    public string Plate { get; set; }
+    public string ParkedTime { get; set; }
+}
---
+++ FreePlace.API/ParkingLots/Resources/SaveCarResource.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreePlace.API.Shared.Resources;
+
+public class SaveCarResource
+{
+    [Required]
+    [MaxLength(10)]
+    public string Plate { get; set; }
+    
+    [Required]
+    public int ParkedTime { get; set; }
+}
