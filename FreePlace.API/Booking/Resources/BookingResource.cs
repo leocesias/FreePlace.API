1dae41b0193587d713ea7f1766b86a5b7d3314bf
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2022-11-08 16:00:14 -0500

    feat(Booking)
    Added Booking resources

---
+++ FreePlace.API/Booking/Resources/BookingResource.cs
@@ -0,0 +1,8 @@
+﻿namespace FreePlace.API.Booking.Resources;
+
+public class BookingResource
+{
+    public int Id { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
