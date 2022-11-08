040155b2d294bf10f21d1433b9ad2246461a82c4
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2022-11-08 15:59:42 -0500

    feat(Booking)
    Added Booking Repository

---
+++ FreePlace.API/Booking/Domain/Repositories/IBookingRepository.cs
@@ -0,0 +1,12 @@
+﻿using FreePlace.API.Booking.Domain.Models;
+
+namespace FreePlace.API.Booking.Domain.Repositories;
+
+public interface IBookingRepository
+{
+    Task<IEnumerable<Booked>> ListAsync();
+    Task AddAsync(Booked booked);
+    Task<Booked> FindByIdAsync(int id);
+    void Update(Booked booked);
+    void Remove(Booked booked);
+}
---
+++ FreePlace.API/Booking/Persistence/Repositories/BookingRepository.cs
@@ -0,0 +1,39 @@
+﻿using FreePlace.API.Booking.Domain.Models;
+using FreePlace.API.Booking.Domain.Repositories;
+using FreePlace.API.Shared.Domain.Repositories;
+using FreePlace.API.Shared.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreePlace.API.Booking.Persistence.Repositories;
+
+public class BookingRepository: BaseRepository, IBookingRepository
+{
+    public BookingRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Booked>> ListAsync()
+    {
+        return await _context.Bookings.ToListAsync();
+    }
+
+    public async Task AddAsync(Booked booked)
+    {
+        await _context.Bookings.AddAsync(booked);
+    }
+
+    public async Task<Booked> FindByIdAsync(int id)
+    { 
+        return await _context.Bookings.FindAsync(id);
+    }
+
+    public void Update(Booked booked)
+    {
+        _context.Bookings.Update(booked);
+    }
+
+    public void Remove(Booked booked)
+    {
+        _context.Bookings.Remove(booked);
+    }
+}
