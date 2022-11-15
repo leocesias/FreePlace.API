b0b3715423b13e5d0255bb20d635ee6e69cd70f7
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2022-11-15 15:27:14 -0500

    feat(review): Added review to DbContext and added relationships in User and DbContext

--- FreePlace.API/Shared/Domain/Models/User.cs
+++ FreePlace.API/Shared/Domain/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using FreePlace.API.Booking.Domain.Models;
 using FreePlace.API.ParkingLots.Domain.Models;
 
 namespace FreePlace.API.Shared.Domain.Models;
@@ -11,13 +12,18 @@ public class User
     public short Age { set; get; }
     public long Phone { set; get; }
     public string Username { set; get; }
+    public bool Suscribed { set; get; } // True or False
+    public bool TypeAccount { set; get; } // True represents Parking owner and False represents Normal User account
 
     [JsonIgnore]
     public string PasswordHash { set; get; }
-    
+
     //Relationships
 
-    public IList<Parking> Parkings { set; get; } = new List<Parking>();
-    public IList<Car> Cars { set; get; } = new List<Car>();
+    public IList<Review> Reviews;
+    public IList<Booked> Bookings;
+    public IList<Parking> Parkings;
+    public IList<Car> Cars;
+    public Car Car;
     // Missing add Security, Booking and History ---
 }
