f0973d4286e4b26081197b87256ed92252d7743f
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2022-11-17 14:19:34 -0500

    feat(payment): Added Payment model in Booking folder

---
+++ FreePlace.API/Booking/Domain/Models/Payment.cs
@@ -0,0 +1,16 @@
+﻿using FreePlace.API.Shared.Domain.Models;
+
+namespace FreePlace.API.Booking.Domain.Models;
+
+public class Payment
+{
+    public int TransactionId { set; get; }
+    public string Description { set; get; }
+    public float Value = 30;
+
+    //Relationships
+
+    public long UserId { set; get; }
+    public User User { set; get; }
+    
+}
