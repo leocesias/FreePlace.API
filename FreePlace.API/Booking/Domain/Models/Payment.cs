4f95490a9e15917cd60ab3dd7b8fe728971cdd84
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2022-11-17 14:27:41 -0500

    feat(payment): Added payment repository interface

--- FreePlace.API/Booking/Domain/Models/Payment.cs
+++ FreePlace.API/Booking/Domain/Models/Payment.cs
@@ -7,6 +7,7 @@ public class Payment
     public int Id { set; get; }
     public string Description { set; get; }
     public float Value = 30;
+    public bool Status { set; get; }
 
     //Relationships
 
